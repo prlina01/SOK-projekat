d36b947901c28d1d14a8412b03b7e666a2b6a6e4
prlina01 <prlinap@gmail.com>
2026-07-07 10:15:00 +0200

    feat: implement graph data models

---
+++ SOK_Graph_Project/api/graph/api/model/graph.py
@@ -0,0 +1,155 @@
+
+from .edge import Edge
+from .node import Node
+
+class Graph:
+
+    def __init__(self, nodes=None, edges=None, cyclic=None, directed=None):
+        self.nodes = nodes
+        self.edges = edges
+        self.cyclic = cyclic
+        self.directed = directed
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+    @nodes.setter
+    def nodes(self, value):
+        self._nodes = self._validateNodesList(value)
+
+    @property
+    def edges(self):
+        return self._edges
+
+    @edges.setter
+    def edges(self, value):
+        self._edges = self._validateEdgesList(value)
+
+    @property
+    def cyclic(self):
+        return self._cyclic
+
+    @cyclic.setter
+    def cyclic(self, value):
+        self._cyclic = value
+
+    @property
+    def directed(self):
+        return self._directed
+
+    @directed.setter
+    def directed(self, value):
+        self._directed = value
+
+    def addNode(self, node):
+        self.nodes.append(self._validateNode(node))
+
+    def addEdge(self, edge):
+        self.edges.append(self._validateEdge(edge))
+
+    def removeEdgesByNode(self, node):
+        matched_edges = [
+            edge for edge in self.edges
+            if edge.getNode1() == node or edge.getNode2() == node
+        ]
+
+        if not matched_edges:
+            raise ValueError("No edges found for the provided node")
+
+        for edge in matched_edges:
+            self.edges.remove(edge)
+
+    def removeEdgeByNodes(self, node1, node2):
+        matched_edges = [
+            edge for edge in self.edges
+            if (edge.getNode1() == node1 and edge.getNode2() == node2)
+            or (edge.getNode1() == node2 and edge.getNode2() == node1)
+        ]
+
+        if not matched_edges:
+            raise ValueError("No edge found for the provided two nodes")
+
+        for edge in matched_edges:
+            self.edges.remove(edge)
+
+    def removeEdge(self, edge):
+        if edge not in self.edges:
+            raise ValueError("Provided edge does not exist in graph")
+
+        self.edges.remove(edge)
+
+    def removeNode(self, node):
+        if node not in self.nodes:
+            raise ValueError("Provided node does not exist in graph")
+
+        if self._isNodeConnected(node):
+            raise ValueError("Cannot remove node because it has connections")
+
+        self.nodes.remove(node)
+
+    def removeNodeByIndex(self, index):
+        target_node = None
+
+        for node in self.nodes:
+            if node.getIndex() == index:
+                target_node = node
+                break
+
+        if target_node is None:
+            raise ValueError("No node found for the provided index")
+
+        if self._isNodeConnected(target_node):
+            raise ValueError("Cannot remove node because it has connections")
+
+        self.nodes.remove(target_node)
+
+    def _isNodeConnected(self, node):
+        for edge in self.edges:
+            if edge.getNode1() == node or edge.getNode2() == node:
+                return True
+
+        return False
+    
+    def getConnectedOf(self, node):
+        matched_nodes = [
+            edge.getNode2() for edge in self.edges
+            if edge.getNode1() == node
+        ]
+        return matched_nodes
+
+    def _validateNodesList(self, nodes):
+        if nodes is None:
+            return []
+
+        if not isinstance(nodes, list):
+            raise TypeError("nodes must be a list of Node objects")
+
+        for node in nodes:
+            self._validateNode(node)
+
+        return nodes
+
+    def _validateEdgesList(self, edges):
+        if edges is None:
+            return []
+
+        if not isinstance(edges, list):
+            raise TypeError("edges must be a list of Edge objects")
+
+        for edge in edges:
+            self._validateEdge(edge)
+
+        return edges
+
+    def _validateNode(self, node):
+        if not isinstance(node, Node):
+            raise TypeError("node must be of type Node")
+
+        return node
+
+    def _validateEdge(self, edge):
+        if not isinstance(edge, Edge):
+            raise TypeError("edge must be of type Edge")
+
+        return edge
---
+++ SOK_Graph_Project/api/graph/api/model/node.py
@@ -0,0 +1,45 @@
+
+
+class Node:
+
+    def __init__(self, data=None, index=None):
+        self._data = data
+        self._index = index
+    
+    @property
+    def data(self):
+        return self._data
+    
+    @data.setter
+    def data(self, value):
+        self._data = self._normalizeData(value)
+
+    @property
+    def index(self):
+        return self._index
+
+    @index.setter
+    def index(self, value):
+        self._index = value
+
+    def addData(self, key, value):
+        self.data[key] = value
+
+    def removeData(self, key):
+        if key not in self.data:
+            raise KeyError(f"Key '{key}' does not exist in node data")
+        del self.data[key]
+
+    def toString(self):
+        return f"Node(data={self.data}, index={self.index})"
+
+    def __str__(self):
+        return self.toString()
+
+    def _normalizeData(self, data):
+        if data is None:
+            return {}
+        try:
+            return dict(data)
+        except Exception as exc:
+            raise ValueError("Node data must be a dictionary or dictionary-compatible value") from exc
